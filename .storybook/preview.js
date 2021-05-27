972caa2030b22e286d4d5283aa8b99c477bc53c0
Ivan Poberejnuk <vanyapoberejnuk@gmail.com>
2021-05-28 00:15:56 +0300

    feat: styled Button component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@material-ui/core';
+import { addDecorator } from '@storybook/react';
+import { withThemes } from '@react-theming/storybook-addon';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import theme from '../src/theme';
+
+const providerFn = ({ theme, children }) => {
+  const muTheme = createMuiTheme(theme);
+  console.log(muTheme);
+  return <ThemeProvider theme={muTheme}>{children}</ThemeProvider>;
+};
+
+addDecorator(withThemes(null, [theme], { providerFn }));
