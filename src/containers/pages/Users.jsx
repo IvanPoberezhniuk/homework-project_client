bba75d10911811cc88ca4c97807d2a259c73f27f
Ivan Poberejnuk <vanyapoberejnuk@gmail.com>
2021-06-08 20:38:50 +0300

    feat: users page

--- src/containers/pages/Users.jsx
+++ src/containers/pages/Users.jsx
@@ -1,16 +1,11 @@
-import { Grid, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Button } from '../../components/button/Button';
 import UsersTable from '../../components/usersTable/UsersTable';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: '0 0 40px 0',
-  },
-  buttonRoot: {
-    width: '220px',
-    textTransform: 'none',
   },
 }));
 
@@ -19,17 +14,9 @@ const Users = () => {
 
   return (
     <>
-      <Grid
-        container
-        direction='row'
-        justify='space-between'
-        className={classes.root}
-      >
-        <Typography variant='h5' component='h1'>
-          Projects
-        </Typography>
-        <Button color='primary' className={classes.buttonRoot}>Create Project</Button>
-      </Grid>
+      <Typography variant='h5' component='h1' className={classes.root}>
+        Users
+      </Typography>
       <UsersTable />
     </>
   );
