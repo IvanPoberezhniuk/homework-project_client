cbb9ce5925171e9a5ed20cbee9c37b3aa8ee0423
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-06-11 14:35:55 +0300

    feat: add sign in and sign up pages

---
+++ src/components/Forms/SigninForm/SigninForm.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { makeStyles } from '@material-ui/core/styles';
+
+import style from './SigninForm.module.css'
+import { Input } from '../../Input/Input';
+import Checkbox from '../../checkbox/Checkbox';
+import { Button } from '../../Button/Button';
+
+const useStyles = makeStyles((theme) => ({
+  checkbox: {
+    display: 'block',
+    width: '100%',
+    textAlign: 'right',
+    margin: '16px 0 0 0',
+    lineHeight: '16px',
+  },
+  checkboxLabel: {
+    fontSize: '16px',
+    color: '#777777',
+  },
+  btn: {
+    width: '322px',
+    height: '37px',
+    marginTop: '40px',
+  },
+}));
+
+const SigninForm = () => {
+  const classes = useStyles();
+
+  const SigninSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Please enter your email address'),
+    password: Yup.string()
+      .min(6, 'Incorrect password')
+      .max(20, 'Incorrect password')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/, 'Invalid password')
+      .required('Please enter your password'),
+  })
+
+  return (
+     <Formik
+       initialValues={{ email: '', password: '', rememberMe: false }}
+       validationSchema={SigninSchema}
+       onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+       }}
+     >
+      <Form>
+        <Input name='email' type='email' placeholder='Email' className={style.inputWrapper} />
+        <Input name='password' type='password' placeholder='Password' className={style.inputWrapper} />
+        <FormControlLabel
+          control={<Checkbox name='rememberMe' /> }
+          label='Remember me?'
+          classes={{ root: classes.checkbox, label: classes.checkboxLabel }
+          }
+        />
+        <Button type='submit' color='primary' classes={{root: classes.btn }}> Sign in</Button>
+       </Form>
+     </Formik>
+   );
+ };
+
+export default SigninForm;
---
+++ src/components/Forms/SignupForm/SignupForm.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { makeStyles } from '@material-ui/core/styles';
+
+import style from './SignupForm.module.css';
+import { Input } from '../../Input/Input';
+import { Button } from '../../Button/Button';
+
+const useStyles = makeStyles(() => ({
+  btn: {
+    width: '322px',
+    height: '37px',
+    marginTop: '40px',
+  },
+}));
+
+const SignupForm = () => {
+  const classes = useStyles();
+
+  const SigninSchema = Yup.object({
+    firstName: Yup.string().max(255, 'Too long').required('Please enter your first name'),
+    lastName: Yup.string().max(255, 'Too long').required('Please enter your last name'),
+    email: Yup.string().email('Invalid email address').required('Please enter your email address'),
+    password: Yup.string()
+      .min(6, 'Incorrect password')
+      .max(20, 'Incorrect password')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/, 'Invalid password')
+      .required('Please enter your password'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Please confirm your password')
+      .required('Please confirm your password'),
+  });
+
+  return (
+     <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      }}
+       validationSchema={SigninSchema}
+       onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+       }}
+     >
+      <Form>
+        <Input name='firstName' type='text' placeholder='First Name' className={style.inputWrapper} />
+        <Input name='lastName' type='text' placeholder='Last Name' className={style.inputWrapper} />
+        <Input name='email' type='email' placeholder='Email' className={style.inputWrapper} />
+        <Input name='password' type='password' placeholder='Password' className={style.inputWrapper} />
+        <Input name='confirmPassword' type='password' placeholder='Confirm Password' className={style.inputWrapper} />
+        <Button type='submit' color='primary' classes={{root: classes.btn }}>Sign Up</Button>
+       </Form>
+     </Formik>
+  );
+};
+
+export default SignupForm;
