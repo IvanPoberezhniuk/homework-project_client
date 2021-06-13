7a22fe684d0ae9c746cb3d40eb226ef804d6eba0
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-06-13 16:41:19 +0300

    feat: add team modal

---
+++ src/components/Modals/TeamModal.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+import { Button, Avatar } from '../index';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    background: '#bfbfbf',
+  },
+  paper: {
+    padding: '40px 32px',
+    margin: 0,
+    borderRadius: '6px',
+    boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.25)',
+  },
+  titleWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    padding: '0 0 0 10px',
+    '& .MuiTypography-h6': {
+      lineHeight: '42px',
+      fontWeight: 400,
+      fontSize: '36px',
+    },
+  },
+  contentWrapper: {
+    padding: '32px 0',
+    textAlign: 'center',
+  },
+  actions: {
+    padding: 0,
+  },
+  actionsSpacing: {
+    '& > :not(:first-child)': {
+      marginLeft: '33px',
+    },
+  },
+  btn: {
+    width: '322px',
+  },
+  itemWrapper: {
+    padding: '8px 0',
+  }
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='down' ref={ref} {...props} />;
+});
+
+const TeamModal = ({ teamList, ...props }) => {
+  const [open, setOpen] = React.useState(true);
+  const classes = useStyles();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  let team = teamList.map((employee) =>
+    <div className={classes.itemWrapper}>
+      <Avatar
+        firstName={employee.firstName}
+        lastName={employee.lastName}
+        isShowName={true}
+      />
+    </div>
+  )
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby='alert-dialog-slide-title'
+        aria-describedby='alert-dialog-slide-description'
+        classes={{ paper: classes.paper, container: classes.container }}
+      >
+        <DialogTitle
+          id='alert-dialog-slide-title'
+          classes={{ root: classes.title }}
+        >
+          Team
+        </DialogTitle>
+        <DialogContent classes={{ root: classes.contentWrapper }}>
+          {team}
+        </DialogContent>
+        <DialogActions
+          classes={{ root: classes.actions, spacing: classes.actionsSpacing }}
+        >
+          <Button color='primary' classes={{ root: classes.btn }} onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default TeamModal;
