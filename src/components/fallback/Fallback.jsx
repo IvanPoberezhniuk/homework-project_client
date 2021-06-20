c1f9bf326c3159d0e7df988ea087b11bb325aace
Ivan Poberejnuk <vanyapoberejnuk@gmail.com>
2021-06-20 22:20:59 +0300

    fix: fallback paddding

--- src/components/fallback/Fallback.jsx
+++ src/components/fallback/Fallback.jsx
@@ -1,6 +1,16 @@
 import Loader from 'react-loaders';
 import './Fallback.scss';
+import { makeStyles } from '@material-ui/styles';
 
-const Fallback = () => <Loader type='pacman' active></Loader>;
+const useStyles = makeStyles(() => ({
+  root: {
+    paddingLeft: '30px',
+  },
+}));
+
+const Fallback = () => {
+  const classes = useStyles();
+  return <Loader className={classes.root} type='pacman' active></Loader>;
+};
 
 export default Fallback;
